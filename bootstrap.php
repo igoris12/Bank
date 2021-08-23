954bd9016ef4c1ae707d3d47a3db91247b34d190
Igoris Ivanovas <igoris.x@inbox.lt>
2021-08-23 21:05:46 +0300

    Bank 2.0 migration

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+define('INSTALL', '/Bank/public/');
+define('DIR',  __DIR__.'/');
+define('URL',  'http://localhost/Bank/public/');
+
+require DIR.'vendor/autoload.php';
---
+++ view/creatNewAcount.php
@@ -0,0 +1,23 @@
+
+<?php 
+ require __DIR__ . '/top.php';
+?>
+
+<div class='containerr'>
+  <div class="creatContainer">
+    <form action="<?=URL?>creat" method="post">
+      <div class='inputContent'>
+        <label >Nmae: </label><input  type="text" name="firstName" placeholder="First name">
+        <label >Last name: </label><input  type="text" name="lastName" placeholder="Last name">
+        <label >Personal code: </label><input  type="text" name="pesonCode" placeholder="Personal code">
+        <label >Nr: </label><input  type="text" name="acNumber" value="#">
+      </div>
+      
+      <button type="submit">Nauja Saskaita</button>
+    </form>
+  </div>
+  
+</div>
+
+
+<?php require __DIR__ . '/bottom.php' ?>
