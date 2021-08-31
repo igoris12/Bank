201ae983426717eb7ff893d25c36dd60d67caef9
Igoris Ivanovas <igoris.x@inbox.lt>
2021-08-31 13:55:51 +0300

    save acces to DB

--- app/MariaDB.php
+++ app/MariaDB.php
@@ -13,6 +13,7 @@ use \PDO;
 
 class MariaDB implements DataBase {
     private static $obj;
+    private $pdo;
 
     public static function get() 
     {
@@ -33,27 +34,12 @@ class MariaDB implements DataBase {
         PDO::ATTR_EMULATE_PREPARES   => false,
         ];
 
-        $pdo = new PDO($dsn, $user, $pass, $options);
+         $this->pdo = new PDO($dsn, $user, $pass, $options);
     }
 
     //
     public function create(array $userData) : void
     {
-        $host = '127.0.0.1';
-        $db   = 'bank';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-
-        $pdo = new PDO($dsn, $user, $pass, $options);
-
         $name = $userData['name'];
         $lastName = $userData['lastName'];
         $personCode = $userData['personCode'];
@@ -62,10 +48,10 @@ class MariaDB implements DataBase {
 
          $sql = "INSERT INTO accounts
             (`name`, lastName, personCode, aNumber, balance)
-            VALUES ('$name', '$lastName', $personCode, '$aNumber', $balance)
+            VALUES (?, ?, ?, ?, ?)
             ";
-
-        $pdo->query($sql);
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([ $name,  $lastName, $personCode, $aNumber, $balance]);
 
     }
  
@@ -73,51 +59,22 @@ class MariaDB implements DataBase {
     {
         $balance = $userData['balance'];
 
-        $host = '127.0.0.1';
-        $db   = 'bank';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-
-        $pdo = new PDO($dsn, $user, $pass, $options);
-
         $sql = "UPDATE accounts
-        SET `balance`= $balance
-        WHERE `id`= $userId
+        SET `balance`= ?
+        WHERE `id`= ?
         ";
-
-        $pdo->query($sql);
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([ $balance,  $userId]);
+    
     }
  
     public function delete(int $userId) : void 
     {
-        $host = '127.0.0.1';
-        $db   = 'bank';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-
-        $pdo = new PDO($dsn, $user, $pass, $options);
-
         $sql = "DELETE FROM accounts
-        WHERE `id` = $userId
+        WHERE `id` = ?
         ";
-
-        $pdo->query($sql);
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([ $userId ]);
     }
  
     public function show(int $userId) : array 
@@ -134,26 +91,11 @@ class MariaDB implements DataBase {
     
     public function showAll() : array 
     {
-        $host = '127.0.0.1';
-        $db   = 'bank';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-
-        $pdo = new PDO($dsn, $user, $pass, $options);
-
         $sql = "SELECT id, `name`, lastName, personCode, aNumber, balance
         FROM accounts
         ";
 
-        $stmt = $pdo->query($sql);
+        $stmt = $this->pdo->query($sql);
         $accounts=[];
         while ($row = $stmt->fetch()) {
             $accounts[] = $row;
